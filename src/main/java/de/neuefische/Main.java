bc0e621ac9fe446b9ccf3d94d517176a35f857ff
Dominic <andro5@web.de>
2024-08-23 15:13:51 +0200

    add information hiding and boilerplate Code

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -5,19 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
         Person pommes = new Person("Müller", 35, "1234567890", true);
-        Person rotWeiß = new Person("Bauer", 23, "1234567890");
+        Person pommes2 = new Person("Müller", 35, "1234567890", true);
+        Person rotWeiss = new Person("Bauer", 23, "1234567890");
 
 
-        Person[] phonebook = {pommes, rotWeiß};
+        Person[] phonebook = {pommes, rotWeiss};
 
         addPerson(pommes);
-        addPerson(rotWeiß);
+        addPerson(rotWeiss);
+
+        System.out.println(pommes.equals(pommes2));
     }
 
     public static void addPerson(Person currywurst){
-        System.out.println(currywurst.name);
-        System.out.println(currywurst.age);
-        System.out.println(currywurst.isAlive);
-        System.out.println(currywurst.name);
+        System.out.println(currywurst.hashCode());
     }
 }
--- src/main/java/de/neuefische/Person.java
+++ src/main/java/de/neuefische/Person.java
@@ -1,24 +1,83 @@
 package de.neuefische;
 
+import java.util.Objects;
+
 public class Person {
 
-   public String name;
-   public int age;
-   public String phone;
-   public boolean isAlive;
+   //Information Hiding
+   private String name;
+   private int age;
+   private String phone;
+   private boolean alive;
 
    //Constructor
    public Person(String name, int age, String phone, boolean isAlive){
       this.name = name;
       this.age = age;
       this.phone = phone;
-      this.isAlive = isAlive;
+      this.alive = isAlive;
    }
-
+   //Method Overloading
    public Person(String name, int age, String phone){
       this.name = name;
       this.age = age;
       this.phone = phone;
-      this.isAlive = true;
+      this.alive = true;
+   }
+
+   //GETTER -> Lesezugriff für Datenfelder
+   public String getName(){
+      return name;
+   }
+   public String getPhone(){
+      return phone;
+   }
+   public int getAge(){
+      return age;
+   }
+   public boolean isAlive(){
+      return alive;
+   }
+
+   //SETTER -> Schreibzugriff
+   public void setName(String name){
+      this.name = name;
+   }
+
+   public void setAge(int age) {
+      this.age = age;
+   }
+
+   public void setPhone(String phone) {
+      this.phone = phone;
+   }
+
+   public void setAlive(boolean alive) {
+      this.alive = alive;
+   }
+
+   @Override
+   public String toString(){
+      return "Person{name= "+name +
+              ", age= "+age+
+              ", phone= "+phone+
+              ", alive? = "+alive;
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Person person = (Person) o; //Typecasting
+      return age == person.age && alive == person.alive && Objects.equals(name, person.name) && Objects.equals(phone, person.phone);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(name, age, phone, alive);
+   }
+
+   public void sayHello(){
+      System.out.println("Hello!");
    }
 }
